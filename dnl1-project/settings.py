f4a27dc1ca0a7510ab3e638975e76dd6150db432
shteniovych7 <shteniovych7@gmail.com>
2019-08-24 10:44:58 +0300

    new .htaccess

--- dnl1-project/settings.py
+++ dnl1-project/settings.py
@@ -10,13 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os, requests
-proxyDict = {
-              "http"  : os.environ.get('FIXIE_URL', ''),
-              "https" : os.environ.get('FIXIE_URL', '')
-            }
-r = requests.get('http://www.dnl1.if.ua', proxies=proxyDict)
-
+import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
