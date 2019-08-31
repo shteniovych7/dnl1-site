152af0bb31904a3a979f02a7af715c1e9960fe6a
shteniovych7 <shteniovych7@gmail.com>
2019-08-31 15:22:42 +0300

    Translated admin panel to UA

--- photogallery/apps.py
+++ photogallery/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class PhotogalleryConfig(AppConfig):
     name = 'photogallery'
+    verbose_name = 'Фотогалерея'
--- photogallery/models.py
+++ photogallery/models.py
@@ -4,19 +4,23 @@ from imagekit.processors import ResizeToFill
 import datetime
 
 class Topic(models.Model):
-    name = models.CharField(max_length=80)
-    date = models.DateField(blank=True, default=datetime.date.today)
+    name = models.CharField('Назва', max_length=80)
+    date = models.DateField('Дата', blank=True, default=datetime.date.today)
 
     objects = models.Manager()
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Альбом'
+        verbose_name_plural = 'Альбоми'
+
 
 class Photo(models.Model):
-    description = models.CharField(max_length=100, null = True, blank = True)
+    description = models.CharField('Опис', max_length=100, null = True, blank = True)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='photogallery/static/photogallery')
+    image = models.ImageField('Фото', upload_to='photogallery/static/photogallery')
     image_thumbnail = ImageSpecField(source='image', 
                                     processors=[ResizeToFill(255,170)],
                                     format='JPEG',
