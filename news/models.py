da035b54f3b0c01a9ae62453a866945be3b09755
shteniovych7 <shteniovych7@gmail.com>
2020-01-27 23:06:30 +0200

    Telegram bot to post news3

--- news/models.py
+++ news/models.py
@@ -8,12 +8,14 @@ from ckeditor.fields import RichTextField
 from photogallery.models import Topic
 
 def post_event_on_telegram(event):
-        message_html = render_to_string('telegram_message.html', {
-            'event': event
-        })
-        telegram_settings = settings.TELEGRAM
-        bot = telegram.Bot(token=telegram_settings['bot_token'])
-        bot.send_message(chat_id="@%s" % telegram_settings['channel_name'], text=message_html, parse_mode=telegram.ParseMode.HTML)          
+    message_html = render_to_string('news/telegram_message.html', {
+        'event': event
+    })
+    message_html = message_html.replace("<p>","").replace("</p>","\n")
+    print(message_html)
+    telegram_settings = settings.TELEGRAM
+    bot = telegram.Bot(token=telegram_settings['bot_token'])
+    bot.send_message(chat_id="@%s" % telegram_settings['channel_name'], text=message_html, parse_mode=telegram.ParseMode.HTML)          
 
 class Article(models.Model):
     title = models.CharField('Назва', max_length = 40) 
