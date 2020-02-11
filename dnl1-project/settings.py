f31d6a27f64e187173066f3c68a23d8ada176ac8
shteniovych7 <shteniovych7@gmail.com>
2020-02-11 20:12:32 +0200

    checking bugs

--- dnl1-project/settings.py
+++ dnl1-project/settings.py
@@ -162,7 +162,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Amazon S3 settings
-if  DEBUG:
+if not DEBUG:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "") 
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
