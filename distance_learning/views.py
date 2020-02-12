1c13cc6aa7cccccea5452b185b9479741d390b1d
shteniovych7 <shteniovych7@gmail.com>
2020-02-12 21:21:07 +0200

    sorted classes

--- distance_learning/views.py
+++ distance_learning/views.py
@@ -4,6 +4,7 @@ import collections
 
 def distance_learning(request):
     tasks_arr = Task.objects.all()
-    tasks_unord = { int(''.join(list(filter(str.isdigit, x.class_name)))) : x for x in tasks_arr}
-    tasks = collections.OrderedDict(sorted(tasks_unord.items()))
-    return render(request, 'distance_learning/distance_learning.html', {'tasks':tasks})
+    t = { x : int(''.join(list(filter(str.isdigit, x.class_name)))) for x in tasks_arr}
+    t1 = {k: v for k, v in sorted(t.items(), key=lambda item: item[1])}
+    print(t1)
+    return render(request, 'distance_learning/distance_learning.html', {"tasks" : t1})
