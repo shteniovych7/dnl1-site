27e1726046ba183d77cfbaa1165e7bd76efbb53f
shteniovych7 <shteniovych7@gmail.com>
2020-03-01 17:58:19 +0200

    achivements page recreate

--- achievements/views.py
+++ achievements/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from achievements.models import AllMedalist
+from achievements.models import AllMedalist, ContestYear
 
 def achievements(request):
     medalists = AllMedalist.objects.all()
     size = len(medalists)
+    contest_winners_by_years = ContestYear.objects.all()
 
-    return render(request, 'achievements/achievements.html', {'medalists': medalists, 'size': size})
+    return render(request, 'achievements/achievements.html', {'medalists': medalists, 'size': size, 'contest_winners_by_years': contest_winners_by_years})
